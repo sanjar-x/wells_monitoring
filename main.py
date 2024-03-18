cfb703fb3c443b9ae132cd06a30869c11f1e6a68
sanjar-x <sanjar68x@gmail.com>
2024-03-18 05:10:37 +0500

    fix

--- main.py
+++ main.py
@@ -102,4 +102,4 @@ async def init_database():
 
 if __name__ == "__main__":
     asyncio.run(init_database())
-    run("main:app", host="0.0.0.0", port=HTTP_PORT, reload=True)
+    # run("main:app", host="0.0.0.0", port=HTTP_PORT, reload=True)
